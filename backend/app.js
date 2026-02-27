28ef00f8338b036f32043da8a246e50b137ffc0c
Aditya <adityaalba27@gmail.com>
2026-02-27 19:20:20 +0530

    Setting up backend and installing dependencies

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+// mongoose.connect(process.env.MONGODB_URI)
+//     .then(() => {
+//         console.log("Connected to MongoDB");
+//     })
+//     .catch((error) => {
+//         console.error("Error connecting to MongoDB:", error);
+//     });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
